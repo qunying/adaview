a8cbe624f3a10d152d3935a646aa208beb80855c
Zhu Qun-Ying <zhu.qunying@gmail.com>
2014-04-16 23:59:53 -0700

    more ghostscript/gdevdsp definition

--- src/gsapi/gs-display_device.ads
+++ src/gsapi/gs-display_device.ads
@@ -59,6 +59,10 @@ with Interfaces.C;
 --  open, presize, memalloc, memfree, close
 --
 
+with Interfaces.C;         use Interfaces.C;
+with System;
+with Interfaces.C.Strings;
+
 package GS.Display_Device is
 
    use Interfaces;
@@ -96,22 +100,22 @@ package GS.Display_Device is
    -- Display_Colors_RGB and Display_Colors_CMYK,
    -- or the depth per pixel for Display_Colors_Native
    -- Display_Depth_2 and Display_Depth_12 have not been tested.
-   Display_Depth_1      : constant display_format_t := Shift_Left (1, 8);
-   Display_Depth_2      : constant display_format_t := Shift_Left (1, 9);
-   Display_Depth_4      : constant display_format_t := Shift_Left (1, 10);
-   Display_Depth_8      : constant display_format_t := Shift_Left (1, 11);
-   Display_Depth_12     : constant display_format_t := Shift_Left (1, 12);
-   Display_Depth_16     : constant display_format_t := Shift_Left (1, 13);
+   Display_Depth_1  : constant display_format_t := Shift_Left (1, 8);
+   Display_Depth_2  : constant display_format_t := Shift_Left (1, 9);
+   Display_Depth_4  : constant display_format_t := Shift_Left (1, 10);
+   Display_Depth_8  : constant display_format_t := Shift_Left (1, 11);
+   Display_Depth_12 : constant display_format_t := Shift_Left (1, 12);
+   Display_Depth_16 : constant display_format_t := Shift_Left (1, 13);
    -- unused Shift_Left (1, 14) and Shift_Left (1 15);
-   Display_Depth_Mask   : constant display_format_t := 16#ff00#;
+   Display_Depth_Mask : constant display_format_t := 16#ff00#;
 
    -- Define whether Red/Cyan should come first,
    -- or whether Blue/Black should come first
-   Display_Bigendian    : constant display_format_t := 0;
+   Display_Bigendian : constant display_format_t := 0;
    -- Red/Cyan first
    Display_Littleendian : constant display_format_t := Shift_Left (1, 16);
    -- Blue/Black first
-   Display_Endian_Mask  : constant display_format_t := 16#1_0000#;
+   Display_Endian_Mask : constant display_format_t := 16#1_0000#;
 
    --  Define whether the raster starts at the top or bottom of the bitmap
    Display_Top_First    : constant display_format_t := 0;
@@ -123,12 +127,69 @@ package GS.Display_Device is
    -- Display_Row_Align_1 : constant display_format_t := Shift_Left (1, 20);
    -- Display_Row_Align_2 : constant display_format_t := Shift_Left (2, 20);
    -- not currently possible for the above two
-   Display_Row_Align_4  : constant display_format_t := Shift_Left (3, 20);
-   Display_Row_Align_8  : constant display_format_t := Shift_Left (4, 20);
-   Display_Row_Align_16 : constant display_format_t := Shift_Left (5, 20);
-   Display_Row_Align_32 : constant display_format_t := Shift_Left (6, 20);
-   Display_Row_Align_64 : constant display_format_t := Shift_Left (7, 20);
+   Display_Row_Align_4    : constant display_format_t := Shift_Left (3, 20);
+   Display_Row_Align_8    : constant display_format_t := Shift_Left (4, 20);
+   Display_Row_Align_16   : constant display_format_t := Shift_Left (5, 20);
+   Display_Row_Align_32   : constant display_format_t := Shift_Left (6, 20);
+   Display_Row_Align_64   : constant display_format_t := Shift_Left (7, 20);
    Display_Row_Align_Mask : constant display_format_t := 16#70_0000#;
+
+   type disp_cb_t is access function
+     (handle : System.Address;
+      device : System.Address)
+      return   int;
+   type disp_presize_cb_t is access function
+     (handle : System.Address;
+      device : System.Address;
+      width  : int;
+      height : int;
+      raster : int;
+      format : unsigned)
+      return   int;
+   type disp_size_cb_t is access function
+     (handle : System.Address;
+      device : System.Address;
+      width  : int;
+      height : int;
+      raster : int;
+      format : unsigned;
+      pimage : access unsigned_char)
+      return   int;
+   type disp_page_cb_t is access function
+     (handle : System.Address;
+      device : System.Address;
+      copies : int;
+      flush  : int)
+      return   int;
+
+   type disp_update_cb_t is access function
+     (handle : System.Address;
+      device : System.Address;
+      x      : int;
+      y      : int;
+      w      : int;
+      h      : int)
+      return   int;
+   type disp_memalloc_cb_t is access function
+     (handle : System.Address;
+      device : System.Address;
+      size   : unsigned_long)
+      return   System.Address;
+   type disp_memfree_cb_t is access function
+     (handle : System.Address;
+      device : System.Address;
+      mem    : System.Address)
+      return   int;
+   type disp_separation_cb_t is access function
+     (handle         : System.Address;
+      device         : System.Address;
+      component      : int;
+      component_name : Interfaces.C.Strings.chars_ptr;
+      c              : unsigned_short;
+      m              : unsigned_short;
+      y              : unsigned_short;
+      k              : unsigned_short)
+      return           int;
 
    type display_callback_t is record
       size : int;
@@ -144,7 +205,65 @@ package GS.Display_Device is
       -- The minor version number will change if new features are added
       -- without changes to this structure.  For example, a new color
       -- format.
+      display_open : disp_cb_t;
+      -- New device has been opened.
+      -- This is the first event from this device.
 
+      display_preclose : disp_cb_t;
+      -- Device is about to be closed.
+      -- Device will not be closed until this function returns.
+
+      display_close : disp_cb_t;
+      -- Device has been closed.
+      -- This is the last event from this device.
+
+      display_presize : disp_presize_cb_t;
+      -- Device is about to be resized.
+      -- Resize will only occur if this function returns 0.
+      -- raster is byte count of a row.
+
+      display_size : disp_size_cb_t;
+      -- Device has been resized.
+      -- New pointer to raster returned in pimage.
+
+      display_sync : disp_cb_t;
+       -- flushpage
+
+      display_page : disp_page_cb_t;
+      -- showpage.
+      -- If you want to pause on showpage, then don't return immediately
+
+      display_update : disp_update_cb_t;
+      -- Notify the caller whenever a portion of the raster is updated.
+      -- This can be used for cooperative multitasking or for
+      -- progressive update of the display.
+      -- This function pointer may be set to NULL if not required.
+
+      display_memalloc : disp_memalloc_cb_t;
+      -- Allocate memory for bitmap
+      -- This is provided in case you need to create memory in a special
+      -- way, e.g. shared.  If this is NULL, the Ghostscript memory device
+      -- allocates the bitmap. This will only called to allocate the
+      -- image buffer. The first row will be placed at the address
+      -- returned by display_memalloc.
+
+      display_memfree : disp_memfree_cb_t;
+      -- Free memory for bitmap
+      -- If this is NULL, the Ghostscript memory device will free the bitmap
+
+      display_separation : disp_separation_cb_t;
+      --  Added in V2
+      -- When using separation color space (DISPLAY_COLORS_SEPARATION),
+      -- give a mapping for one separation component.
+      -- This is called for each new component found.
+      -- It may be called multiple times for each component.
+      -- It may be called at any time between display_size
+      -- and display_close.
+      -- The client uses this to map from the separations to CMYK
+      -- and hence to RGB for display.
+      -- GS must only use this callback if version_major >= 2.
+      -- The unsigned short c,m,y,k values are 65535 = 1.0.
+      -- This function pointer may be set to NULL if not required.
    end record;
    pragma Convention (C, display_callback_t);
    -- Note that for Windows, the display callback functions are
