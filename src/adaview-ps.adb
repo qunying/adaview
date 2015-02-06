ea6f5c916b92c34067cd86fffef096d70daea18d
zhu Qun-Ying <zhu.qunying@gmail.com>
2015-02-05 18:01:15 -0800

    try to convert ps.c from gv to Ada

--- src/adaview-ps.adb
+++ src/adaview-ps.adb
@@ -19,14 +19,52 @@
 -- along with this program; if not, see <http://www.gnu.org/licenses/>.      --
 -------------------------------------------------------------------------------
 
--- using routings from psscan from libspectre
--- and add back handling of PDF from gv/psscan
+with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
+with Ada.Directories;
+
+with GNATCOLL.Mmap; use GNATCOLL.Mmap;
 
 package body Adaview.PS is
 
-   procedure Scan (Ctx : in out Context_T) is
+   type File_Data_T is record
+      File : Mapped_File;
+      File_Size : Ada.Directories.File_Size;
+      Str  : Str_Access;
+      Offs : Long_Integer := 0;
+
+      Line_Begin : Long_Integer := 0;
+      Line_End   : Long_Integer := 0;
+      Line_Len   : Long_Integer := 0;
+      Status     : Boolean := True; -- True for OK, False for failed
+   end record;
+
+   ---------------------------------------------------------------------------
+   procedure IO_Init (File_Name : String, File : in out File_Data_T) is
+   begin
+      File.File := Open_Read (File_Name);
+      File.File_Size := Ada.Directories.Size (File_Name);
+   end IO_Init;
+
+   ---------------------------------------------------------------------------
+   function Read_Line (File : File_Data_T) return Boolean is
    begin
       null;
+   end Read_line;
+
+   ---------------------------------------------------------------------------
+   procedure Scan (Ctx : in out Context_T) is
+      File : File_Data_T;
+   begin
+      if Length (Ctx.Cur_Doc.DCS_Name) > 0 then
+         -- use the DCS file created from previous run
+         IO_Init (To_String (Ctx.Cur_Doc.DCS_Name), File);
+      else
+         IO_Init (To_String (Ctx.Cur_Doc.Temp_Name), File);
+      end if;
+
+      Read_Line (File);
+
+      Close (File.File);
    end Scan;
 
 end Adaview.PS;
--- src/adaview-ps.ads
+++ src/adaview-ps.ads
@@ -19,10 +19,122 @@
 -- along with this program; if not, see <http://www.gnu.org/licenses/>.      --
 -------------------------------------------------------------------------------
 
+-- Postscript scanning and copying routines.
+-- Translated from the ps.c file from gv to suite the need of Adaview.
+
 with Adaview.Config; use Adaview.Config;
 
 package Adaview.PS is
 
    procedure Scan (Ctx : in out Context_T);
+   -- scan the PostScript file for document structuring comments.
+   --
+   -- This scanner is designed to retrieve the information necessary for
+   -- the ghostview previewer.  It will scan files that conform to any
+   -- version (1.0, 2.0, 2.1, or 3.0) of the document structuring conventions.
+   -- It does not really care which version of comments the file contains.
+   -- (The comments are largely upward compatible.)  It will scan a number
+   -- of non-conforming documents.  (You could have part of the document
+   -- conform to V2.0 and the rest conform to V3.0.  It would be similar
+   -- to the DC-2 1/2+, it would look funny but it can still fly.)
+   --
+   -- This routine returns a pointer to the document structure.
+   -- The structure contains the information relevant to previewing.
+   -- These include EPSF flag (to tell if the file is a encapsulated figure),
+   -- Page Media (for the Page Size), Bounding Box (to minimize backing
+   -- pixmap size or determine window size for encapsulated PostScript),
+   -- Orientation of Paper (for default transformation matrix), and
+   -- Page Order.  The title and CreationDate are also retrieved to
+   -- help identify the document.
+   --
+   -- The following comments are examined:
+   --
+   -- Header section:
+   -- Must start with %!PS-Adobe-.  Version numbers ignored.
+   --
+   -- %!PS-Adobe-* [EPSF*] (changed EPSF-* to EPSF* to do XFig a favor ...###jp###)
+   -- %%BoundingBox: <int> <int> <int> <int>|(atend)
+   -- %%CreationDate: <textline>
+   -- %%Orientation: Portrait|Landscape|(atend)
+   -- %%Pages: <uint> [<int>]|(atend)
+   -- %%PageOrder: Ascend|Descend|Special|(atend)
+   -- %%Title: <textline>
+   -- %%DocumentMedia: <text> <real> <real> <real> <text> <text>
+   -- %%DocumentPaperSizes: <text>
+   -- %%EndComments
+   --
+   -- Note: Either the 3.0 or 2.0 syntax for %%Pages is accepted.
+   --       Also either the 2.0 %%DocumentPaperSizes or the 3.0
+   --       %%DocumentMedia comments are accepted as well.
+   --
+   -- The header section ends either explicitly with %%EndComments or
+   -- implicitly with any line that does not begin with %X where X is
+   -- a not whitespace character.
+   --
+   -- If the file is encapsulated PostScript the optional Preview section
+   -- is next:
+   --
+   -- %%BeginPreview
+   -- %%EndPreview
+   --
+   -- This section explicitly begins and ends with the above comments.
+   --
+   -- Next the Defaults section for version 3 page defaults:
+   --
+   -- %%BeginDefaults
+   -- %%PageBoundingBox: <int> <int> <int> <int>
+   -- %%PageOrientation: Portrait|Landscape
+   -- %%PageMedia: <text>
+   -- %%EndDefaults
+   --
+   -- This section explicitly begins and ends with the above comments.
+   --
+   -- The prolog section either explicitly starts with %%BeginProlog or
+   -- implicitly with any nonblank line.
+   --
+   -- %%BeginProlog
+   -- %%EndProlog
+   --
+   -- The Prolog should end with %%EndProlog, however the proglog implicitly
+   -- ends when %%BeginSetup, %%Page, %%Trailer or %%EOF are encountered.
+   --
+   -- The Setup section is where the version 3 page defaults are found.
+   -- This section either explicitly begins with %%BeginSetup or implicitly
+   -- with any nonblank line after the Prolog.
+   --
+   -- %%BeginSetup
+   -- %%PageBoundingBox: <int> <int> <int> <int>
+   -- %%PageOrientation: Portrait|Landscape
+   -- %%PaperSize: <text>
+   -- %%EndSetup
+   --
+   -- The Setup should end with %%EndSetup, however the setup implicitly
+   -- ends when %%Page, %%Trailer or %%EOF are encountered.
+   --
+   -- Next each page starts explicitly with %%Page and ends implicitly with
+   -- %%Page or %%Trailer or %%EOF.  The following comments are recognized:
+   --
+   -- %%Page: <text> <uint>
+   -- %%PageBoundingBox: <int> <int> <int> <int>|(atend)
+   -- %%PageOrientation: Portrait|Landscape
+   -- %%PageMedia: <text>
+   -- %%PaperSize: <text>
+   --
+   -- The trailer section start explicitly with %%Trailer and end with %%EOF.
+   -- The following comment are examined with the proper (atend) notation
+   -- was used in the header:
+   --
+   -- %%Trailer
+   -- %%BoundingBox: <int> <int> <int> <int>|(atend)
+   -- %%Orientation: Portrait|Landscape|(atend)
+   -- %%Pages: <uint> [<int>]|(atend)
+   -- %%PageOrder: Ascend|Descend|Special|(atend)
+   -- %%EOF
+   --
+   --
+   -- + A DC-3 received severe damage to one of its wings.  The wing was a total
+   --  loss.  There was no replacement readily available, so the mechanic
+   --  installed a wing from a DC-2.
+
 
 end Adaview.PS;
